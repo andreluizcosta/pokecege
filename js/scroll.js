9e8abd69f5acc5d5ebbe7111f707d639af7ed3c0
Andre Luiz <andreluiz.r.costa@hotmail.com>
2020-10-30 14:46:50 -0300

    First Commit

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+//window.addEventListener("scroll", () => {
+//    const { scrollTop, scrollHeight, clientHeight}
+//    = document.documentElement;
+//
+//    console.log({scrollTop, scrollHeight, clientHeight});
+//
+//    if( scrollTop >= 1000){ //clientHeight + scrollTop >= scrollHeight - 100
+//        console.log("to the bottom");
+//        //show the loading animation
+//    }
+//});
